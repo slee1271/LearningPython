d590394a5f3835d07a1ec5d30c6fc26f0ff98c13
Shawn Lee <slee1271@mtroyal.ca>
2024-09-12 17:37:18 -0600

    done 4

--- 4.Input.py
+++ 4.Input.py
@@ -3,7 +3,9 @@
 name = input("What is your name?: ")
 age = input("How old are you?: ")
 age = int(age) #should change to int because cannot use for arithmetic
+# can reduce lines by typing int(input("How old are you?: "))
 
 print(f"Hello {name}")
 print(f"You are {age} years old")
 print(f"You are turning {age + 1} next!")
+
