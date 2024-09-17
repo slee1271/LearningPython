54971cc6b56693780b309b800802033f3846adb8
Shawn Lee <slee1271@mtroyal.ca>
2024-09-16 23:42:12 -0600

    while loops

---
+++ 13.WhileLoops.py
@@ -0,0 +1,25 @@
+# while loops execute some code WHILE some condition remains true 
+
+# Example 1
+# name = input("Enter your name: ")
+# while name == "":
+#     print("You did not enter your name")
+#     name = input("Enter your name: ") # this line is necessary or else there will be an infinite loop
+# print(f"Hello {name}")
+
+
+# Example 2
+# food = input("Enter a food you like (q to quit): ")
+# while not food == "q":
+#     print(f"You like {food}")
+#     food = input("Enter a food you like (q to quit): ")
+# print("Bye")
+
+# Example 3
+num = int(input("Enter a num between 1-10: "))
+while num < 1 or num > 10:
+    print(f"{num} is not valid")
+    num = int(input("Enter a num between 1-10: "))
+print(f"Your number is: {num}")
+
+    
