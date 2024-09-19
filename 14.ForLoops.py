1230ce3287048192f166d1fbea07ce2f4ebc8fcc
Shawn Lee <slee1271@mtroyal.ca>
2024-09-18 21:08:53 -0600

    for loops

---
+++ 14.ForLoops.py
@@ -0,0 +1,16 @@
+# for loops execute a block a code a fixed number of time. You can iterate over a range, string, sequence, etc
+# for x in range (inclusive, exclusive, step)
+
+for x in reversed(range(0,11)): # counts backwords
+    print(x)
+print("Happy New Year")
+
+credit_card = "1234-5678-9012-3456"
+for x in credit_card:
+    print(x)
+
+for x in range (1,20):
+    if x == 13: # skips 13 bc its an unlucky number
+        continue
+    else: 
+        print(x)
