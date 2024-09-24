0e63a661e46014aa1687035023dc72e8e4bf2f0c
Shawn Lee <slee1271@mtroyal.ca>
2024-09-23 19:48:53 -0600

    error commit with functions practice

--- 20.FunctionPractice.py
+++ 20.FunctionPractice.py
@@ -1,9 +1,11 @@
-# def display_invoice(username, amount, due_date):
-#     print(f"Hello {username}")
-#     print(f"Your bill of ${amount:.2f} is due: {due_date}")
+def display_invoice(username, amount, due_date):
+    print(f"Hello {username}")
+    print(f"Your bill of ${amount:.2f} is due: {due_date}")
 
-# display_invoice("Shawn", 123.083, "January 1st")
+display_invoice("Shawn", 123.083, "January 1st")
 
+
+# functions with return statements
 def create_name(first,last):
     first = first.capitalize()
     last = last.capitalize()
