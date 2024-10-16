3f93ca5ef2eb5e7447feef5c77f34bb6f74601c8
Shawn Lee <slee1271@mtroyal.ca>
2024-10-16 17:17:28 -0600

    pushing

--- 30.IfNameMain.py
+++ 30.IfNameMain.py
@@ -1,3 +1,7 @@
 # if __name__ == __main__: (this script can be imported or run standalone)
 # functions and classes in this module can be reused without the main block of code executing
 
+def main():
+
+if __name__ == '__main__':
+    main()
